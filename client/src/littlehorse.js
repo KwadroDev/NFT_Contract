ab740840c611e24392269899fbdf9beb97b0f768
KwadroDev <97489426+KwadroDev@users.noreply.github.com>
2022-01-18 22:00:50 -0400

    edit ip storage3

--- client/src/littlehorse.js
+++ client/src/littlehorse.js
@@ -253,7 +253,7 @@ const setSaleActive = async() => {
 }
 
 const setBaseURI = async() => {
-    const result = await contract.methods.setBaseURI("http://147.182.160.69/")
+    const result = await contract.methods.setBaseURI("http://147.182.160.69:80/")
         .send({ from: accounts[0], gas: 0, value: 0 })
         .on('transactionHash', function(hash) {
             document.getElementById("web3_message").textContent = "Minting...";
